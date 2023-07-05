802a13b01508ef5f526cc34b6a7ba788e5037606
Gee Chai <fc00004@gmail.com>
2023-07-05 15:28:04 +0900

    feat: navigation with no refresh

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,36 @@
 import React from "react";
 import "../styles/header.css";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+interface CustomLinkProps {
+  to: string;
+  children: React.ReactNode;
+}
 
 function Header() {
   return (
     <nav className="navbar">
-      <a href="/" className="site-title">
+      <Link to="/" className="site-title">
         Ass Nordre Tolerud
-      </a>
+      </Link>
       <ul>
-        <li className="">
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/menu">Menu</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
+        <CustomLink to="/menu">Menu</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
+        <CustomLink to="/contact">Contact</CustomLink>
       </ul>
     </nav>
+  );
+}
+
+function CustomLink({ to, children, ...props }: CustomLinkProps) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
   );
 }
 
