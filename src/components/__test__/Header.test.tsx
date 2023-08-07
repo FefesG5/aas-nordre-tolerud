08119f01222b8b216dffa3179db82bc14d5159d8
Gee Chai <fc00004@gmail.com>
2023-08-07 19:08:56 +0900

    fet: basic header test

--- src/components/__test__/Header.test.tsx
+++ src/components/__test__/Header.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { BrowserRouter } from "react-router-dom";
 import Header from "../Header";
+import { useTranslation } from "react-i18next";
 
 interface Translations {
   "Aas Nordre Tolerud": string;
@@ -12,31 +13,65 @@ interface Translations {
   NO: string;
 }
 
-jest.mock("react-i18next", () => ({
-  useTranslation: () => ({
-    t: (key: keyof Translations) => {
-      const translations = {
-        "Aas Nordre Tolerud": "Your translated site title",
-        About: "Your translated About",
-        Menu: "Your translated Menu",
-        Contact: "Your translated Contact",
-        EN: "Your translated EN",
-        NO: "Your translated NO",
-      };
-      return translations[key] || key;
-    },
-    i18n: { language: "en" },
-  }),
-}));
-
-describe("Header", () => {
-  it("renders the translated site title", () => {
+describe("Header - English", () => {
+  beforeEach(() => {
+    jest.resetModules();
     render(
       <BrowserRouter>
         <Header />
       </BrowserRouter>
     );
+  });
+
+  it("renders the site title name 'Aas Nordre Tolerud' ", () => {
     const siteTitleElement = screen.getByText(/Aas Nordre Tolerud/i);
     expect(siteTitleElement).toBeInTheDocument();
   });
+
+  it("renders the translated site title 'About' navigation in English", () => {
+    const siteTitleElement = screen.getByRole("link", { name: /About/i });
+    expect(siteTitleElement).toBeInTheDocument();
+  });
+
+  it("renders the translated site title 'Menu' navigation in English", () => {
+    const siteTitleElement = screen.getByText(/Menu/i);
+    expect(siteTitleElement).toBeInTheDocument();
+  });
+
+  it("renders the translated site title 'Contact' navigation in English", () => {
+    const siteTitleElement = screen.getByText(/Contact/i);
+    expect(siteTitleElement).toBeInTheDocument();
+  });
 });
+
+// describe("Header - Norwegian", () => {
+//   beforeEach(() => {
+//     jest.resetModules();
+//     useTranslation().i18n.changeLanguage("no");
+//     render(
+//       <BrowserRouter>
+//         <Header />
+//       </BrowserRouter>
+//     );
+//   });
+
+//   it("renders the site title name 'Aas Nordre Tolerud' ", () => {
+//     const siteTitleElement = screen.getByText(/Aas Nordre Tolerud/i);
+//     expect(siteTitleElement).toBeInTheDocument();
+//   });
+
+//   it("renders the translated site title 'About' navigation in Norwegian", () => {
+//     const siteTitleElement = screen.getByText(/Om oss/i);
+//     expect(siteTitleElement).toBeInTheDocument();
+//   });
+
+//   it("renders the translated site title 'Menu' navigation in Norwegian", () => {
+//     const siteTitleElement = screen.getByText(/Meny/i);
+//     expect(siteTitleElement).toBeInTheDocument();
+//   });
+
+//   it("renders the translated site title 'Contact' navigation in Norwegian", () => {
+//     const siteTitleElement = screen.getByText(/Kontakt/i);
+//     expect(siteTitleElement).toBeInTheDocument();
+//   });
+// });
