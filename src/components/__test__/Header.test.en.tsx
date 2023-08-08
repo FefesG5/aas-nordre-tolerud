ae9751a02ac1f295c6a1ea38058369b81cd87bfb
Gee Chai <fc00004@gmail.com>
2023-08-08 18:44:22 +0900

    feat: header test english

---
+++ src/components/__test__/Header.test.en.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { BrowserRouter } from "react-router-dom";
+import Header from "../Header";
+
+describe("Header - English", () => {
+  beforeEach(() => {
+    jest.resetModules();
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    );
+  });
+
+  it("renders the site title name 'Aas Nordre Tolerud' ", () => {
+    const siteTitleElement = screen.getByText(/Aas Nordre Tolerud/i);
+    expect(siteTitleElement).toBeInTheDocument();
+  });
+
+  it("renders the translated site title 'About' navigation in English", () => {
+    const siteTitleElement = screen.getByRole("link", { name: /About/i });
+    expect(siteTitleElement).toBeInTheDocument();
+  });
+
+  it("renders the translated site title 'Menu' navigation in English", () => {
+    const siteTitleElement = screen.getByText(/Menu/i);
+    expect(siteTitleElement).toBeInTheDocument();
+  });
+
+  it("renders the translated site title 'Contact' navigation in English", () => {
+    const siteTitleElement = screen.getByText(/Contact/i);
+    expect(siteTitleElement).toBeInTheDocument();
+  });
+});
