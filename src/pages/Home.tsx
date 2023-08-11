2eeec08ad31138758a8b5c553604cb13db96b9c1
Gee Chai <fc00004@gmail.com>
2023-08-11 18:47:56 +0900

    feat: homepage css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,14 @@ function Home() {
   const { t } = useTranslation();
   return (
     <div className="home-container">
-      <h2>{t("AasNordreTolerudGard")}</h2>
-      <p className="home-text">{t("homeText")}</p>
+      <div className="background-image">
+        <h1 className="image-heading">{t("headline")}</h1>
+      </div>
+      <div className="overlay"></div>
+      <div className="text-container">
+        <h2 className="home-heading">{t("AasNordreTolerudGard")}</h2>
+        <p className="home-text">{t("homeText")}</p>
+      </div>
     </div>
   );
 }
