3edd79594c3f066c146fbc69cdae7500d40aa3d7
Gee Chai <fc00004@gmail.com>
2023-08-21 22:21:29 +0900

    feat: footer translations

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,35 +9,34 @@ function Footer() {
     <>
       <div className="footer-container">
         @ 2023 Gee Chai | {t("allRightsReserved")}
-        <div className="attribution">
+        <div className="attribution-link flag-attribution-container">
           <a
             href="https://www.flaticon.com/free-icons/norway"
             title="norway icons"
             target="_blank"
             rel="noopener noreferrer"
-            className="attribution-link"
+            className="flaticon-link"
           >
             <img src={NorwayIcon} className="footer-flag-icon" alt="" />
-            Norway icons created by GeekClick - Flaticon
           </a>
           <a
             href="https://www.flaticon.com/free-icons/uk"
             title="uk icons"
             target="_blank"
             rel="noopener noreferrer"
-            className="attribution-link"
+            className="flaticon-link"
           >
             <img src={UnitedKingdomIcon} className="footer-flag-icon" alt="" />
-            Uk icons created by Roundicons - Flaticon
           </a>
-          <div className="">
-            <a
-              href="https://unsplash.com/photos/vIm26fn_QKg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-              className="attribution-link"
-            >
-              Panzanella Image taken by Luisa Brimble - Unsplash
-            </a>
-          </div>
+          <p>{t("geekClickIconsAttributionText")}</p>
+        </div>
+        <div>
+          <a
+            href="https://unsplash.com/photos/vIm26fn_QKg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+            className="attribution-link"
+          >
+            {t("panzanellaAttributionText")}
+          </a>
         </div>
       </div>
     </>
