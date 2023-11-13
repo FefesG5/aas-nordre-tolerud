b4fc7aa5a23b6ec334613c80c9df148f500129d5
Gee Chai <fc00004@gmail.com>
2023-11-13 16:54:42 +0900

    refactor(scrollBtn): debounce function on the scrollUp

--- src/components/ScrollBtn.tsx
+++ src/components/ScrollBtn.tsx
@@ -1,11 +1,21 @@
 import { useEffect, useState } from "react";
 import UpArrow from "../images/UpArrow.png";
+import { debounce } from "../utils/debounce";
 import "../styles/scrollBtn.css";
 
 const scrollPositionYThreshold = 400;
 
 export default function ScrollBtn() {
   const [showScrollBtn, setShowScrollBtn] = useState(false);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const debouncedScroll = debounce(scrollToTop, 500);
 
   useEffect(() => {
     const handleScrollUp = () => {
@@ -17,20 +27,13 @@ export default function ScrollBtn() {
     return () => window.removeEventListener("scroll", handleScrollUp);
   }, []);
 
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
-
   if (!showScrollBtn) {
     return null;
   }
 
   return (
     <button
-      onClick={scrollToTop}
+      onClick={debouncedScroll}
       className="icon-position icon-style"
       aria-label="Scroll to top"
     >
