7024f2a1144480a7f17160544a5c41a9640323a0
Gee Chai <fc00004@gmail.com>
2023-11-17 14:57:04 +0900

    about(new image): added extra image to gallery, reduced the image size

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -10,10 +10,17 @@ import ScrollBtn from "../components/ScrollBtn";
 import GingerHouseView from "../images/AasNordreTolerud/GingerHouseView.png";
 import GingerHouseViewThumbnail from "../images/AasNordreTolerud/GingerHouseViewThumbnail.png";
 
+import GingerHouseBack2 from "../images/AasNordreTolerud/GingerHouseBack2.png";
+import GingerHouseBack2Thumbnail from "../images/AasNordreTolerud/GingerHouseBack2Thumbnail.png";
+
 const images = [
   {
     original: GingerHouseView,
     thumbnail: GingerHouseViewThumbnail,
+  },
+  {
+    original: GingerHouseBack2,
+    thumbnail: GingerHouseBack2Thumbnail,
   },
 ];
 
