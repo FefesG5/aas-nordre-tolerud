784ef337ba1d676b843742690971e05e32b84319
Gee Chai <fc00004@gmail.com>
2023-12-25 11:15:13 +0900

    chore: install & configure tailwind.css

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import i18n from "./i18n";
 import { I18nextProvider } from "react-i18next";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
